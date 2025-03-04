cf50ace678d154186770aeebd38c0c4d2e53c299
edard3v <edard3v@gmail.com>
2025-03-04 10:39:19 -0500

    agg galería

---
+++ src/views/Home/Galery/galery.ts
@@ -0,0 +1,51 @@
+import img_1 from "./img/1.avif";
+import img_2 from "./img/2.avif";
+import img_3 from "./img/3.avif";
+import img_5 from "./img/5.avif";
+import img_6 from "./img/6.avif";
+import img_8 from "./img/8.avif";
+import img_9 from "./img/9.avif";
+import img_10 from "./img/10.avif";
+
+export const GALERY = [
+  {
+    id: crypto.randomUUID(),
+    name: "img_1",
+    img: img_1,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_2",
+    img: img_2,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_3",
+    img: img_3,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_5",
+    img: img_5,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_6",
+    img: img_6,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_8",
+    img: img_8,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_9",
+    img: img_9,
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "img_10",
+    img: img_10,
+  },
+];
