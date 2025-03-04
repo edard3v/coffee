246b3419beaa621ccf935a96f0cb04e003442670
edard3v <edard3v@gmail.com>
2025-03-04 11:37:20 -0500

    agg cloudfler adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
+  adapter: cloudflare(),
+  output: "static",
 });
 
 // vite: {
